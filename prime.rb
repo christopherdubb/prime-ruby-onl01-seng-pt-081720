f654a99117f81d3ff3ae9eed6131564056499a33
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-31 00:36:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,34 @@
+require 'benchmark'
+require 'bigdecimal/math'
+
+# calculate pi to 10k digits
+puts Benchmark.measure { BigMath.PI(10_000) }
+
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
+
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  counter = 0
+  primes.each do |p|
+   
+    next unless p
+
+    break if p*p > max
+    counter += 1
+  
+    
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
 def prime?(num)
   sieve(num).include?(num)
 end
