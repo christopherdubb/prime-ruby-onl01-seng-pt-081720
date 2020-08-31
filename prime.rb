3198a9bf684441db0b85f216eca503d954f377c4
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-31 00:37:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,8 @@ require 'bigdecimal/math'
 puts Benchmark.measure { BigMath.PI(10_000) }
 
 def sieve(max)
-  # Set up an array with all the numbers from 0 to the max
+ 
   primes = (0..max).to_a
-
-  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
-  # aren't prime.
   primes[0] = primes[1] = nil
 
   counter = 0
@@ -24,7 +21,6 @@ def sieve(max)
     (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  # Finally, return the compacted array.
   puts "Solved for #{max} in #{counter} steps."
   primes.compact
 end
